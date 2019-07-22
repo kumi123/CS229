0880ea47bdf12a3f7f7bfc66ab91a04b3b3aa4d2
MasterDoo <masterdu9951@icloud.com>
2019-07-22 12:14:29 -0700

    update

---
+++ ps2/src/stability/simple_plot.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+
+def plot(x, y, save_path=None):
+    """Visualize dataset for binary classification.
+
+    Args:
+        x: Matrix of training examples, one per row.
+        y: Vector of labels in {0, 1}.
+        save_path: Path to save the plot. If None, plot is shown using GUI.
+    """
+    # Plot dataset
+    plt.figure()
+    plt.plot(x[y == 1, -2], x[y == 1, -1], 'bx', linewidth=2)
+    plt.plot(x[y == 0, -2], x[y == 0, -1], 'go', linewidth=2)
+
+    # Add labels and save to disk
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    if save_path is None:
+        plt.show()
+    else:
+        plt.savefig(save_path)
+
